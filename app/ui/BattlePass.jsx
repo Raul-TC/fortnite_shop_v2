c6290ddd5b22434bbd7abc59fa70d02e0fa9406f
Raul-TC <raultc.9819@gmail.com>
2024-04-24 19:10:52 -0600

    changed Image for img temporally

--- app/ui/BattlePass.jsx
+++ app/ui/BattlePass.jsx
@@ -8,25 +8,24 @@ import Link from 'next/link'
 import Image from 'next/image'
 import { rewardsFiltered } from '../lib/utils'
 import BackgroundCard from './BackgroundCard'
-const BattlePass = ({ bpass,currentPage}) => {
-
+const BattlePass = ({ bpass, currentPage }) => {
   const { arr, info, seasonDates, videos } = bpass
   const { formatedDate } = useFormatedDate()
-  let [date] = new Date(seasonDates.end).toISOString().split('T')
-console.log(arr)
-  let rewards = rewardsFiltered(arr,currentPage)
+  const [date] = new Date(seasonDates.end).toISOString().split('T')
+  console.log(arr)
+  const rewards = rewardsFiltered(arr, currentPage)
   return (
     <>
       <h1 className='text-center font-bold text-2xl'>{info.chapterSeason}</h1>
-      <div className="flex flex-col self-start items-center justify-center w-full rounded-md top-0 left-0 right-0">
+      <div className='flex flex-col self-start items-center justify-center w-full rounded-md top-0 left-0 right-0'>
 
         <CurrentDay date={formatedDate(date)} isShop={false} />
         <CountDown isShop={false} date={seasonDates.end} />
 
       </div>
-      <div className=" flex items-center justify-between w-full">
+      <div className=' flex items-center justify-between w-full'>
         <video
-          className="relative left-0 right-0 top-0 bottom-0 h-full z-10 w-full"
+          className='relative left-0 right-0 top-0 bottom-0 h-full z-10 w-full'
           src={videos[0].url}
           alt={videos[0].type}
           controls
@@ -34,23 +33,24 @@ console.log(arr)
           muted
         />
       </div>
-    <h2 className=' my-4 text-center text-xl font-bold'>Recompensas del pase de batalla</h2>
-      {arr && rewards.map((item,index) => (
+      <h2 className=' my-4 text-center text-xl font-bold'>Recompensas del pase de batalla</h2>
+      {arr && rewards.map((item, index) => (
         <section key={`${item.page}_${index * 2}`} className='pb-4 w-full h-full'>
           <h2 className='text-2xl text-center font-bold mt-4 mb-4 md:text-3xl'> Página {item.page}</h2>
           <div className='text-center grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 lg:grid-cols-6 gap-3 grid-flow-dense h-full w-full'>
-            {item.data.map((pag,index) => (
+            {item.data.map((pag, index) => (
               <Link key={`${pag.offerId}_${index}`} href={`/shop/${pag.item.id}`}>
-                  <div className='relative w-full h-full '>
-                  
-                <Image
-                  src={pag.item.images.background || pag.item.images.icon}
-                  alt={`image_${pag.item.name}`}
-                  width={300}
-                  height={300}
-                  className='w-full h-full rounded-md'
-                  quality={70} />
-              <BackgroundCard displayName={pag.item.name}/>
+                <div className='relative w-full h-full '>
+
+                  <img
+                    src={pag.item.images.background || pag.item.images.icon}
+                    alt={`image_${pag.item.name}`}
+                    width={300}
+                    height={300}
+                    className='w-full h-full rounded-md'
+                    // quality={70}
+                  />
+                  <BackgroundCard displayName={pag.item.name} />
                 </div>
 
               </Link>
