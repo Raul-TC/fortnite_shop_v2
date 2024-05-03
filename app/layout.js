6ee0541f4150ed75cd64fb21ef3a425d7ca539c9
Raul-TC <raultc.9819@gmail.com>
2024-05-02 22:16:13 -0600

    changed font and margin top for layout

--- app/layout.js
+++ app/layout.js
@@ -2,14 +2,14 @@ import Link from 'next/link'
 import './globals.css'
 import './hamburger.css'
 import Header from './ui/Header'
-import { montserrat } from './ui/fonts'
+import { balsamiqSans } from './ui/fonts'
 
 export default function RootLayout ({ children }) {
   return (
     <html lang='es'>
-      <body className={`${montserrat.className} bg-bg-body text-white pb-[80px]`}>
+      <body className={`${balsamiqSans.className} bg-bg-body text-white pb-[80px]`}>
         <Header />
-        <div className='w-[95%] max-w-[1440px] m-auto  mt-[80px] md:min-h-[95vh]'>
+        <div className='w-[95%] max-w-[1440px] m-auto  mt-[100px] md:min-h-[95vh]'>
           {children}
         </div>
         <footer className='w-full text-center fixed bottom-0 h-[40px] mt-[50px] bg-bg-header flex items-center justify-center z-[20] gap-1'>Made by  <Link href='https://github.com/Raul-TC' className='text-red-400'>Raul-TC 💖</Link> </footer>
