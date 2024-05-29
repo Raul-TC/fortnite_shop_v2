1c67d97b3735497f12429f562a5b4f69df660e66
Raul-TC <raultc.9819@gmail.com>
2024-05-29 15:40:09 -0600

    fix property name

--- app/ui/PlayerStats.jsx
+++ app/ui/PlayerStats.jsx
@@ -40,19 +40,105 @@ const PlayerStats = async ({ name, accountType }) => {
         <h2 className={`${luckiestGuy.className} text-center text-3xl`}>{account.name} </h2>
         <h2 className={`${luckiestGuy.className} text-center text-2xl`}>Temporada Actual: Nivel {battlePass.level}</h2>
       </div>
-      {/* <div className='my-4'> */}
       <div className='flex w-full flex-col md:flex-row items-start justify-start flex-wrap self-start gap-4'>
-        {stats.season.solo && <StatCard wins={stats.season.solo?.wins} kills={stats.season.solo?.kills} top10={stats.season.solo?.top10} top25={stats.season.solo?.top25} deaths={stats.season.solo?.deaths} kda={stats.season.solo?.kd} matches={stats.season.solo?.matches} winrate={stats.season.solo?.winRate} timePlayed={handleMinutes(stats.season.solo?.minutesPlayed)} updated={handleLocalDate(stats.season.solo?.lastModified)} modo='Solo' />}
-        <StatCard wins={stats.season.duo?.wins} kills={stats.season.duo.kills} top10={stats.season.duo.top5} top25={stats.season.duo.top12} deaths={stats.season.duo.deaths} kda={stats.season.duo.kd} matches={stats.season.duo.matches} winrate={stats.season.duo.winRate} timePlayed={handleMinutes(stats.season.duo.minutesPlayed)} updated={handleLocalDate(stats.season.duo.lastModified)} modo='Duo' />
-        {stats.season.squad && <StatCard wins={stats.season.squad?.wins ?? 0} kills={stats.season.squad?.kills} top10={stats.season.squad?.top3} top25={stats.season.squad?.top6} deaths={stats.season.squad?.deaths} kda={stats.season.squad?.kd} matches={stats.season.squad?.matches} winrate={stats.season.squad?.winRate} timePlayed={handleMinutes(stats.season.squad?.minutesPlayed)} updated={handleLocalDate(stats.season.squad?.lastModified)} modo='Squad' />}
+        {stats.season.solo &&
+          <StatCard
+            wins={stats.season.solo?.wins}
+            kills={stats.season.solo?.kills}
+            topN={stats.season.solo?.top10}
+            topNN={stats.season.solo?.top25}
+            deaths={stats.season.solo?.deaths}
+            kda={stats.season.solo?.kd}
+            matches={stats.season.solo?.matches}
+            winrate={stats.season.solo?.winRate}
+            timePlayed={handleMinutes(stats.season.solo?.minutesPlayed)}
+            updated={handleLocalDate(stats.season.solo?.lastModified)}
+            modo='Solo'
+          />}
+        <StatCard
+          wins={stats.season.duo?.wins}
+          kills={stats.season.duo.kills}
+          topN={stats.season.duo.top5}
+          topNN={stats.season.duo.top12}
+          deaths={stats.season.duo.deaths}
+          kda={stats.season.duo.kd}
+          matches={stats.season.duo.matches}
+          winrate={stats.season.duo.winRate}
+          timePlayed={handleMinutes(stats.season.duo.minutesPlayed)}
+          updated={handleLocalDate(stats.season.duo.lastModified)}
+          modo='Duo'
+        />
+        {stats.season.squad &&
+          <StatCard
+            wins={stats.season.squad?.wins ?? 0}
+            kills={stats.season.squad?.kills}
+            topN={stats.season.squad?.top3}
+            topNN={stats.season.squad?.top6}
+            deaths={stats.season.squad?.deaths}
+            kda={stats.season.squad?.kd}
+            matches={stats.season.squad?.matches}
+            winrate={stats.season.squad?.winRate}
+            timePlayed={handleMinutes(stats.season.squad?.minutesPlayed)}
+            updated={handleLocalDate(stats.season.squad?.lastModified)}
+            modo='Squad'
+          />}
       </div>
-      {/* </div> */}
       <h2 className={`${luckiestGuy.className} text-center text-5xl my-4`}>Todas las Temporadas</h2>
       <div className='flex w-full flex-col md:flex-row items-center justify-center flex-wrap gap-4'>
-        <StatCard wins={stats.lifetime.solo.wins} kills={stats.lifetime.solo.kills} top10={stats.lifetime.solo.top10} top25={stats.lifetime.solo.top25} deaths={stats.lifetime.solo.deaths} kda={stats.lifetime.solo.kd} matches={stats.lifetime.solo.matches} winrate={stats.lifetime.solo.winRate} timePlayed={handleMinutes(stats.lifetime.solo.minutesPlayed)} updated={handleLocalDate(stats.lifetime.solo.lastModified)} modo='Solo' />
-        <StatCard wins={stats.lifetime.duo.wins} kills={stats.lifetime.duo.kills} top10={stats.lifetime.duo.top5} top25={stats.lifetime.duo.top12} deaths={stats.lifetime.duo.deaths} kda={stats.lifetime.duo.kd} matches={stats.lifetime.duo.matches} winrate={stats.lifetime.duo.winRate} timePlayed={handleMinutes(stats.lifetime.duo.minutesPlayed)} updated={handleLocalDate(stats.lifetime.duo.lastModified)} modo='Duo' />
-        <StatCard wins={stats.lifetime.trio?.placetop1 ?? 0} kills={stats.lifetime.trio?.kills ?? 0} top10={stats.lifetime.trio?.placetop3} top25={stats.lifetime.trio?.placetop6} deaths='' kda={stats.lifetime.trio?.kd} matches={stats.lifetime.trio?.matchesplayed} winrate={stats.lifetime.trio?.winrate} timePlayed={handleMinutes(stats.lifetime.trio.minutesplayed)} updated='Desconocido' modo='Trio' />
-        {stats.lifetime.squad && <StatCard wins={stats.lifetime.squad?.wins ?? 0} kills={stats.lifetime.squad?.kills} top10={stats.lifetime.squad?.top3} top25={stats.lifetime.squad?.top6} deaths={stats.lifetime.squad?.deaths} kda={stats.lifetime.squad?.kd} matches={stats.lifetime.squad?.matches} winrate={stats.lifetime.squad?.winRate} timePlayed={handleMinutes(stats.lifetime.squad?.minutesPlayed)} updated={handleLocalDate(stats.lifetime.squad?.lastModified)} modo='Squad' />}
+        <StatCard
+          wins={stats.lifetime.solo.wins}
+          kills={stats.lifetime.solo.kills}
+          topN={stats.lifetime.solo.top10}
+          topNN={stats.lifetime.solo.top25}
+          deaths={stats.lifetime.solo.deaths}
+          kda={stats.lifetime.solo.kd}
+          matches={stats.lifetime.solo.matches}
+          winrate={stats.lifetime.solo.winRate}
+          timePlayed={handleMinutes(stats.lifetime.solo.minutesPlayed)}
+          updated={handleLocalDate(stats.lifetime.solo.lastModified)}
+          modo='Solo'
+        />
+
+        <StatCard
+          wins={stats.lifetime.duo.wins}
+          kills={stats.lifetime.duo.kills}
+          topN={stats.lifetime.duo.top5}
+          topNN={stats.lifetime.duo.top12}
+          deaths={stats.lifetime.duo.deaths}
+          kda={stats.lifetime.duo.kd}
+          matches={stats.lifetime.duo.matches}
+          winrate={stats.lifetime.duo.winRate}
+          timePlayed={handleMinutes(stats.lifetime.duo.minutesPlayed)}
+          updated={handleLocalDate(stats.lifetime.duo.lastModified)}
+          modo='Duo'
+        />
+        <StatCard
+          wins={stats.lifetime.trio?.placetop1 ?? 0}
+          kills={stats.lifetime.trio?.kills ?? 0}
+          topN={stats.lifetime.trio?.placetop3}
+          topNN={stats.lifetime.trio?.placetop6}
+          deaths=''
+          kda={stats.lifetime.trio?.kd}
+          matches={stats.lifetime.trio?.matchesplayed}
+          winrate={stats.lifetime.trio?.winrate}
+          timePlayed={handleMinutes(stats.lifetime.trio.minutesplayed)}
+          updated='Desconocido'
+          modo='Trio'
+        />
+        {stats.lifetime.squad &&
+          <StatCard
+            wins={stats.lifetime.squad?.wins ?? 0}
+            kills={stats.lifetime.squad?.kills}
+            topN={stats.lifetime.squad?.top3}
+            topNN={stats.lifetime.squad?.top6}
+            deaths={stats.lifetime.squad?.deaths}
+            kda={stats.lifetime.squad?.kd}
+            matches={stats.lifetime.squad?.matches}
+            winrate={stats.lifetime.squad?.winRate}
+            timePlayed={handleMinutes(stats.lifetime.squad?.minutesPlayed)}
+            updated={handleLocalDate(stats.lifetime.squad?.lastModified)}
+            modo='Squad'
+          />}
       </div>
     </div>
   )
--- app/ui/StatCard.jsx
+++ app/ui/StatCard.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
 import { luckiestGuy, balsamiqSans } from './fonts'
 
-const StatCard = ({ wins, kills, top10, top25, deaths, kda, matches, timePlayed, updated, modo }) => {
+const StatCard = ({ wins, kills, topN, topNN, deaths, kda, matches, timePlayed, updated, modo }) => {
   const commonClass = 'pl-3 text-gray-500'
   return (
     <div className={`${balsamiqSans.className} font-bold relative m-auto bg-bg-header text-base h-[342px] pt-[37px] rounded-md w-[98%] md:w-[432px]`}>
       <p className={commonClass}>Ganadas: <span className='text-white'>{wins}</span></p>
       <p className={commonClass}>Asesinatos: <span className='text-white'>{kills}</span></p>
-      {modo === 'Solo' && <p className={commonClass}>Top 10:<span className='text-white'> {top10}</span></p>}
-      {modo === 'Solo' && <p className={commonClass}>Top 25: <span className='text-white'>{top25}</span></p>}
-      {modo === 'Duo' && <p className={commonClass}>Top 5: <span className='text-white'>{top10}</span></p>}
-      {modo === 'Duo' && <p className={commonClass}>Top 12: <span className='text-white'>{top25}</span></p>}
-      {modo === 'Trio' && <p className={commonClass}>Top 3: <span className='text-white'>{top10}</span></p>}
-      {modo === 'Trio' && <p className={commonClass}>Top 6: <span className='text-white'>{top25}</span></p>}
-      {modo === 'Squad' && <p className={commonClass}>Top 3: <span className='text-white'>{top10}</span></p>}
-      {modo === 'Squad' && <p className={commonClass}>Top 6: <span className='text-white'>{top25}</span></p>}
+      {modo === 'Solo' && <p className={commonClass}>Top 10:<span className='text-white'> {topN}</span></p>}
+      {modo === 'Solo' && <p className={commonClass}>Top 25: <span className='text-white'>{topNN}</span></p>}
+      {modo === 'Duo' && <p className={commonClass}>Top 5: <span className='text-white'>{topN}</span></p>}
+      {modo === 'Duo' && <p className={commonClass}>Top 12: <span className='text-white'>{topNN}</span></p>}
+      {modo === 'Trio' && <p className={commonClass}>Top 3: <span className='text-white'>{topN}</span></p>}
+      {modo === 'Trio' && <p className={commonClass}>Top 6: <span className='text-white'>{topNN}</span></p>}
+      {modo === 'Squad' && <p className={commonClass}>Top 3: <span className='text-white'>{topN}</span></p>}
+      {modo === 'Squad' && <p className={commonClass}>Top 6: <span className='text-white'>{topNN}</span></p>}
       {modo !== 'Trio' && <p className={commonClass}>Muertes: <span className='text-white'>{deaths}</span></p>}
       <p className={commonClass}>K / D: <span className='text-white'>{kda}</span></p>
       <p className={commonClass}>Partidas: <span className='text-white'>{matches}</span></p>
